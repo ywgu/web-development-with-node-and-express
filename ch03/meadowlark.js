3f0b152c2b607d9d29c615f7acdab1dce8abd47f
ywgu <ywgu@yahoo.com>
2016-04-06 21:46:51 +0800

    Update meadowlark.js

--- ch03/meadowlark.js
+++ ch03/meadowlark.js
@@ -14,7 +14,7 @@ app.use(express.static(__dirname + '/public'));
 
 var fortuneCookies = [
 	"Conquer your fears or they will conquer you.",
-	"Rivers need springs.",
+	"Rivers need springs..",
 	"Do not fear what you don't know.",
 	"You will have a pleasant surprise.",
 	"Whenever possible, keep it simple.",
